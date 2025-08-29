0bdebc6b591fd0f6327005876217d0efaf2cba4a
ali <7731.aa@gmail.com>
2025-08-29 20:57:45 +0430

    add models for library

--- library/models.py
+++ library/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Member(AbstractUser):
+    user_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Category(models.Model):
+    class CategoryChoices(models.TextChoices):
+        BIOGRAPHIE = "BIO", _("Biographie")
+        BUSINESS = "BUSI", _("Business")
+        COMICS = "COMI", _("Comics")
+        TECHNOLOGY = "TECH", _("Technology")
+        COOKING = "COOK", _("Cooking")
+        EDUCATIONAL = "EDU", _("Educational")
+        ENTERTAINMENT = "ENT", _("Entertainment")
+        HEALTH = "HEA", _("Health")
+        HISTORY = "HIS", _("History")
+        ROMANCE = "ROM", _("Romance")
+        SPORT = "SPO", _("Sport")
+        SCIFI = "SCI", _("Science Fiction")
+
+    name = models.CharField(max_length=100, 
+                            choices=CategoryChoices.choices, 
+                            default=CategoryChoices.BIOGRAPHIE)
+    
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name="Last Name")
+    email = models.EmailField(verbose_name="Email Address")
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Book(models.Model):
+    category = models.ManyToManyField(Category, related_name='book')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
+    favourites = models.ManyToManyField(Member, 
+                                        related_name="favourite", 
+                                        default=None, 
+                                        blank=True)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.FloatField(null=True, blank=True)
+    pages = models.PositiveIntegerField(null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
